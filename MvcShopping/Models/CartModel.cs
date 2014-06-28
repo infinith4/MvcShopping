f40ea4e87c3554661aa821f214f5b3db2d7ec895
infinith4 <infinith4@gmail.com>
2014-06-28 14:38:55 +0900

    なぜ追跡されてないファイルを追加した

---
+++ MvcShopping/Models/CartModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcShopping.Models
+{
+    public class CartModel
+    {
+        public List<CartItem> Items;
+        public CartModel() {
+            this.Items = new List<CartItem>();
+        }
+    }
+    //カートの商品クラス
+    public class CartItem {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+    }
+}
